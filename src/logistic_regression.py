f6045b282be3e00b345b6b52e55e28cd3a630e41
Kenten Danas <kdanas@uw.edu>
2018-05-31 11:35:14 -0700

    test commit. adding python module

---
+++ src/logistic_regression.py
@@ -0,0 +1,8 @@
+"""
+This code consists of my own Python implementation of l2 regularized
+logistic regression.
+
+Implementation by Kenten Danas for DATA 558
+University of Washington, June 2018
+kentdanas@gmail.com
+"""
